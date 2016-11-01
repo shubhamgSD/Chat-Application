7bc50b86b3b37d569a7416c33534505109b5711e
shubhamgSD <shubhamgupta.shiv@gmail.com>
2016-11-01 19:37:01 +0530

    Socket Programming
    Chit chat at two different console.

---
+++ Server.java
@@ -0,0 +1,82 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+class Server {
+	BufferedReader reader;
+	PrintWriter writer;
+	InputStreamReader streamReader;
+	ServerSocket serverSocket;
+	Socket clientSocket;
+	BufferedReader clientInput;
+	
+	public static void main(String[] args)throws Exception {
+		Server server = new Server();
+		server.go();
+
+	}
+	
+	void go() throws Exception
+	{
+		serverSocket = new ServerSocket(4991);
+		try
+		{
+			while(true)
+			{
+				clientSocket = serverSocket.accept();
+				streamReader = new InputStreamReader(clientSocket.getInputStream());
+				reader  = new BufferedReader(streamReader);
+				writer = new PrintWriter(clientSocket.getOutputStream());
+				System.out.println("Server Networking Established:");
+				
+				Thread thread = new Thread(new IncomingReader());
+				thread.start();
+				
+
+				clientInput = new BufferedReader(new InputStreamReader(System.in));
+				String line;
+				while (true) {
+
+					line = clientInput.readLine();// keyboard Reading
+
+					writer.println(line);// sending text to server
+					writer.flush();
+				}
+				
+			
+
+			}
+			
+			
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			
+		}
+		
+	
+	}
+	
+	public class IncomingReader implements Runnable {
+
+		public void run() {
+			String message;
+			try {
+				while ((message = reader.readLine()) != null) {
+					System.out.println("client: " + message);
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+	}
+
+
+}
